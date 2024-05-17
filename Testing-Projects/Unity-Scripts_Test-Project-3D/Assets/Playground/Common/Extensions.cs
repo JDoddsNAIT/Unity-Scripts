93fe8205ccc8f8d33c1281eaea10c166b7289d11
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-05-17 12:03:11 -0600

    added addition and scale extension methods

--- Testing-Projects/Unity-Scripts_Test-Project-3D/Assets/Playground/Common/Extensions.cs
+++ Testing-Projects/Unity-Scripts_Test-Project-3D/Assets/Playground/Common/Extensions.cs
@@ -9,23 +9,27 @@ public static class QuaternionExtensions
     public static Vector3 Up(this Quaternion q) => q * Vector3.up;
     public static Vector3 Down(this Quaternion q) => q * Vector3.down;
 
-    public static Quaternion RotateTowards(this Quaternion from, Vector3 to, float maxDegrees) =>
-        Quaternion.RotateTowards(from, Quaternion.FromToRotation(Vector3.forward, to), maxDegrees);
+    public static Quaternion FromToRotation(this Quaternion from, Quaternion to) => Quaternion.FromToRotation(from.Forward(), to.Forward());
 
-    public static Quaternion LookTowards(this Quaternion from, Vector3 to, Vector3 up, float maxDegrees) =>
-        Quaternion.RotateTowards(from, Quaternion.LookRotation(to, up), maxDegrees);
+    public static Quaternion LookTowards(this Quaternion from, Vector3 toDirection, Vector3 up, float maxDegrees) =>
+        Quaternion.RotateTowards(from, Quaternion.LookRotation(toDirection, up), maxDegrees);
 
-    public static Quaternion LookTowards(this Quaternion from, Vector3 to, float maxDegrees) =>
-        Quaternion.RotateTowards(from, Quaternion.LookRotation(to), maxDegrees);
+    public static Quaternion LookTowards(this Quaternion from, Vector3 toDirection, float maxDegrees) =>
+        Quaternion.RotateTowards(from, Quaternion.LookRotation(toDirection), maxDegrees);
+
+    // Quaternion math
+    public static Quaternion Add(this Quaternion a, Quaternion b) => Quaternion.Euler(a.eulerAngles + b.eulerAngles);
+
+    public static Quaternion Scale(this Quaternion q, float s) => Quaternion.Euler(q.eulerAngles * s);
 }
 
 //public static class TransformExtensions
 //{
-//    public static void LookTowards(this Transform transform, Vector3 to, Vector3 up, float maxDegrees)
+//    public static void LookTowards(this Transform transform, Vector3 toDirection, Vector3 up, float maxDegrees)
 //    {
 //        transform.rotation = Quaternion.RotateTowards(
 //            from: transform.rotation,
-//            to: Quaternion.LookRotation(to, up),
+//            to: Quaternion.LookRotation(toDirection, up),
 //            maxDegrees);
 //    }
 //}
