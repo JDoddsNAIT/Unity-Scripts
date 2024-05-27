30084ddc2e9c20b45eba4c63bc7345e253a44548
JDoddsNAIT <144161210+JDoddsNAIT@users.noreply.github.com>
2024-05-27 11:05:00 -0600

    created 2d project

---
+++ Testing-Projects/Unity-Scripts_Test-Project-2D/Assets/JDoddsNAIT/SplinePath/BezierPath.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[AddComponentMenu("Spline Path/Bezier Path")]
+[HelpURL("https://github.com/JDoddsNAIT/Unity-Scripts/tree/main/dScripts/Follow-Path")]
+public class BezierPath : Path
+{
+    public override void GetPoint(float t, out Vector3 position, out Quaternion? rotation)
+    {
+        t = Mathf.Clamp01(t);
+
+        Vector3 b = new();
+        int n = points.Count - (closeLoop ? 0 : 1);
+
+        for (int i = 0; i <= n; i++)
+        {
+            b += Combination(n, i) * Mathf.Pow(1 - t, n - i) * Mathf.Pow(t, i) * points[i % points.Count].position;
+        }
+        position = b;
+        rotation = null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (PathIsValid)
+        {
+            Gizmos.color = pathColor;
+            float t = 0f;
+            for (int i = 0; i < curveSegments; i++)
+            {
+                GetPoint(t, out var from, out _);
+                t += 1 / (float)curveSegments;
+                GetPoint(t, out var to, out _);
+                Gizmos.DrawLine(from, to);
+            }
+        }
+    }
+
+    public static int Combination(int n, int r) => Factorial(n) / (Factorial(r) * Factorial(n - r));
+    public static int Factorial(int n)
+    {
+        int nf = 1;
+        while (n > 1)
+        {
+            nf *= n;
+            n--;
+        }
+        return nf;
+    }
+}
---
+++ Testing-Projects/Unity-Scripts_Test-Project-2D/Assets/JDoddsNAIT/SplinePath/Path.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[HelpURL("https://github.com/JDoddsNAIT/Unity-Scripts/tree/main/dScripts/Follow-Path")]
+public abstract class Path : MonoBehaviour
+{
+    #region Inspector Values
+    [Header("Gizmo settings")]
+    public Color pathColor = Color.white;
+    [Tooltip("The amount of segments drawn. Ignored if Path Type is Linear.")]
+    [Range(1, 100)] public int curveSegments = 20; 
+    [Header("Path settings")]
+    public bool closeLoop;
+    public List<Transform> points = new();
+    #endregion
+
+    private readonly string INVALID_PATH = $"Length of {nameof(points)} cannot be less than 2 or contain any nulls.";
+    public virtual bool PathIsValid
+    {
+        get
+        {
+            var result = points != null && (points.Count >= 2) && !points.Where(p => p == null).Any();
+            if (!result)
+            {
+                Debug.LogError(INVALID_PATH);
+            }
+            return result;
+        }
+    }
+
+    public abstract void GetPoint(float t, out Vector3 position, out Quaternion? rotation);
+
+    [ContextMenu("Generate Path from Children")]
+    private void UseChildren()
+    {
+        List<Transform> transforms = GetComponentsInChildren<Transform>(includeInactive: false).Where(t => t != transform).ToList();
+        if (transforms.Count > 0)
+        {
+            points = transforms;
+        }
+        else
+        {
+            Debug.Log("Could not generate path as no children were found");
+        }
+    }
+}
